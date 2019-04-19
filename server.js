0d59f0434772221f19aaa2de53e17b76cd7b87be
Hreinn Rúnarsson <hreinnrunarsson@Hreinns-MacBook-Pro.local>
2019-04-19 17:14:08 +0000

    Email & password Login

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /*jshint esversion: 6 */
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -8,12 +9,17 @@ const posts = require('./routes/api/posts');
 
 const app = express();
 
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
 //DB config
 const db = require('./config/keys').mongoURI;
 
 //Connect to MongoDB
 mongoose
-  .connect(db)
+  .connect(db, {useNewUrlParser: true })
   .then(() => console.log('mongodb connected'))
   .catch(err => console.log(err));
 
